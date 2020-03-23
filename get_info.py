87f17069eb88633108156c0e7cb9dad09be72bb4
Samuel.Rison <samuel.rison@cityfibre.com>
2020-03-23 17:38:24 +0000

    Adding the first part allowing to retrieve player information based on name v2

--- get_info.py
+++ get_info.py
@@ -1,15 +1,17 @@
 import io
 
 def search_player(name):
-    profile_id = ""
-    steam_id = ""
+    profile_id = []
+    steam_id = []
     with io.open("player_list.txt", "r", encoding="utf-8") as f:
         for line in f:
             l = line.split(':')
             if name.lower() == l[0].lower():
-                profile_id = l[1]
-                steam_id = l[2]
+                profile_id.append(l[1])
+                steam_id.append(l[2])
 
-search_player("TescoValue13")
-search_player("TheViper")
-search_player("MiniWolskys")
+    return profile_id, steam_id
+
+profile_id_list, steam_id_list = search_player("TescoValue13")
+profile_id_list, steam_id_list = search_player("TheViper")
+profile_id_list, steam_id_list = search_player("MiniWolskys")
